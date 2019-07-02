6e09468cf4de9c1444e7c5955cfe144c3157f8b4
Katie Seggelke <katie.seggelke@gmail.com>
2019-07-02 18:54:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,3 +17,16 @@ def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 
 end
+
+
+
+describe "Building a Nested Monopoly Hash" do
+  describe "Adding Key/Value Pairs in the Top Level" do
+    describe "monopoly" do
+      it "adds a key/value pair to the monopoly hash. The key should be :railroads and the value should be an empty hash" do
+        expect(monopoly[:railroads]).to eq({})
+        expect(monopoly.keys.count).to eq(1)
+        expect(monopoly.values).to match_array([{}])
+      end
+    end
+  end
